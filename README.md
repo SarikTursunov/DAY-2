33ff20931f38aeeb69b902a1e3692d27f9ed4689
Sarik Tursunov <tsa20000901@mail.ru>
2025-07-10 21:20:23 -0400

    commit 1

---
+++ README.md
@@ -0,0 +1,4 @@
+## Day 2 - Creating Calculator
+- Get User Float
+- Get User Second Float
+- Sum it
---
+++ day2c.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    
+
+printf("Enter first float: ");
+    return 0;
+}
